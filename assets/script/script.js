273b2afff9ed4c9717af4d61737e4edfd6da1369
Paulina Arnesson <paulinaarnesson@hotmail.com>
2020-01-10 15:31:05 +0100

    get date and month render names to HTML

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,6 +16,16 @@ const renderDate = (data, name) => {
     `
 };
 
+const renderNames = (data, day, month) => {
+    console.log(data);
+    const names = data.data.map(name => name.namedays.us);
+
+    document.querySelector('#names').innerHTML = `
+    <h2>These names have name day ${day}/${month}:</h2>
+    <p>${names.join(', ')}</p>
+    `
+};
+
 // Add eventlistener
 document.querySelector('#form').addEventListener('submit', e => {
     e.preventDefault();
@@ -45,7 +55,7 @@ document.querySelector('#form').addEventListener('submit', e => {
         .then(data => {
             if(200){
                 console.log(data);
-                renderDate(data, name);
+                renderNames(data, day, month);
             }else{
                 console.error('not OK');
                 //kalla på en warnings funktion med data.message
